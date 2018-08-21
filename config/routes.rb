6c7eeb01a905cba6c9db54af234fc9a3182270f0
Brian Bytnar <bytnarb2@gmail.com>
2018-08-21 13:01:07 -0400

    added root route and site controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'site/welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'site#welcome'
+
 end
